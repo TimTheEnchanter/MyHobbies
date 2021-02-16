f00e234bf59c860fa6167b284d60b7ba420798af
James Parrott <japarrott@shaw.ca>
2021-02-16 16:03:10 -0700

    New Pages
    Add two new pages, got those wired up and set-up a menu.

---
+++ resources/views/info.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                    I'm baby tattooed kinfolk wolf chia, church-key af four dollar toast stumptown raw denim raclette unicorn snackwave waistcoat readymade butcher. Hoodie banjo artisan gochujang vinyl. Blue bottle tousled woke, salvia four dollar toast bitters jean shorts squid. Adaptogen wayfarers hell of selvage occupy brunch seitan heirloom enamel pin glossier keffiyeh fam mustache. La croix fingerstache try-hard farm-to-table hexagon swag. Man bun messenger bag vice hammock, pour-over gochujang hashtag cray chillwave tacos PBR&B bushwick helvetica.
+
+                    Direct trade kinfolk subway tile DIY knausgaard. Cornhole crucifix four loko quinoa, shaman green juice ethical tbh marfa narwhal mustache. Woke fingerstache kickstarter chillwave, ugh health goth ennui wayfarers hoodie. Microdosing vegan hella edison bulb dreamcatcher put a bird on it chartreuse slow-carb tilde venmo DIY tattooed. Small batch offal sartorial pour-over selvage. Literally YOLO lumbersexual live-edge normcore lo-fi intelligentsia pitchfork actually master cleanse hell of fingerstache poke knausgaard.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
