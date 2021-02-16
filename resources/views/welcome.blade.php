7b6f2a46d0d7961c20aaf609f630cfa2a67e9ac2
James Parrott <japarrott@shaw.ca>
2021-02-15 20:26:26 -0700

    Test Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hi there
                 </div>
 
                 <div class="links">
